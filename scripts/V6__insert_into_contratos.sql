c3c4f5019f280462fd4e41375e3f182b5e08e3e0
hectorex <celsohectorhm@gmail.com>
2026-09-07 13:32:22 -0400

    fix: corrige id_cliente dos contratos para apontar para pessoas com eh_cliente

--- scripts/V6__insert_into_contratos.sql
+++ scripts/V6__insert_into_contratos.sql
@@ -5,15 +5,15 @@ ON CONFLICT (numero_contrato) DO NOTHING;
 
 -- Inserir contrato com pagamento via Cartão
 INSERT INTO contratos (numero_contrato, data_contrato, tipo_pagamento, id_cliente, id_veiculo, data_inicio, data_fim)
-VALUES ('CTR-2026-002', '2026-09-05', 'Cartão', 3, 2, '2026-09-05', '2026-09-20')
+VALUES ('CTR-2026-002', '2026-09-05', 'Cartão', 2, 2, '2026-09-05', '2026-09-20')
 ON CONFLICT (numero_contrato) DO NOTHING;
 
 -- Contrato com pagamento via PIX feito por um ATENDENTE que também é CLIENTE
 INSERT INTO contratos (numero_contrato, data_contrato, tipo_pagamento, id_cliente, id_veiculo, data_inicio, data_fim)
-VALUES ('CTR-2026-003', '2026-08-01', 'PIX', 3, 3, '2026-08-01', '2026-08-15')
+VALUES ('CTR-2026-003', '2026-08-01', 'PIX', 4, 3, '2026-08-01', '2026-08-15')
 ON CONFLICT (numero_contrato) DO NOTHING;
 
 -- Contrato antigo já finalizado
 INSERT INTO contratos (numero_contrato, data_contrato, tipo_pagamento, id_cliente, id_veiculo, data_inicio, data_fim)
-VALUES ('CTR-2026-004', '2026-01-10', 'Cartão', 4, 1, '2026-01-10', '2026-01-20')
+VALUES ('CTR-2026-004', '2026-01-10', 'Cartão', 1, 1, '2026-01-10', '2026-01-20')
 ON CONFLICT (numero_contrato) DO NOTHING;
